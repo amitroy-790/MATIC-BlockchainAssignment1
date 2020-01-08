bc23f6c458e05fb17de53692e18bbf66da5b7fdf
amitroy-git <amitroy.790@gmail.com>
2020-01-08 20:38:42 +0530

    adding db service

--- blockIndexer.js
+++ blockIndexer.js
@@ -1,10 +1,11 @@
 const Web3 = require("web3");
+const {addBlock} = require("./services/dbService");
 const rinkebyAPI = 'https://kovan.infura.io/v3/5e66f831443940ed88e9adca82578c2b';
 
 const web3 = new Web3(rinkebyAPI);
 
 web3.eth.getBlockNumber().then((result,err)=>{
     for(i=result-3;i<=result;i++){
-        web3.eth.getBlock(i).then(console.log);
+        web3.eth.getBlock(i).then(addBlock);
     }
 })
