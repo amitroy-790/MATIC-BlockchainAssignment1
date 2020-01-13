17fc417c08c92b1ae84e1043fb65520f95a8d0ca
amitroy-git <amitroy.790@gmail.com>
2020-01-14 00:47:33 +0530

    adding txns to the database

--- blockIndexer.js
+++ blockIndexer.js
@@ -5,19 +5,41 @@ const {kovanrpc,dburl,blockcount,dbname,dbcollection} = require("./param/config"
 
 const web3 = new Web3(kovanrpc);
 
-async function addblock() {
-    console.log('[@Client] triggering the service');
-    var latest = await web3.eth.getBlockNumber();
-    console.log(latest);
+async function addTransactions() {
+
     var db = await MongoClient.connect(dburl);
     var dbo = db.db(dbname);
-    for(i=latest-blockcount;i<=latest;i++){
-        var blockobj = await web3.eth.getBlock(i);
-        dbo.collection(dbcollection).insertOne(blockobj, function(err, res) {
-            if (err) throw err;
-            });
+
+    latest = await web3.eth.getBlockNumber();
+    for(i = latest;i>latest-blockcount;i--){
+        let block = await web3.eth.getBlock(i);
+        let txns = [];
+        if ("transactions" in block === true) {
+            let txhlist = block['transactions'];
+            if (txhlist.length>0) {
+                    await Promise.all(txhlist.map(async (txh) => {
+                        let tx = await web3.eth.getTransaction(txh);
+                        let txobj = new Object({
+                            'hash':tx.hash,
+                            'blockNumber':tx.blockNumber,
+                            'from':tx.from,
+                            'to':tx.to,
+                            'value':tx.value
+                        })
+                        txns.push(txobj);
+                }));
+                dbo.collection(dbcollection).insertMany(txns, function(err, res) {
+                    if (err) 
+                    {
+                        //db error log writter
+                    }
+                });
+            }
+        }
+        console.log(i);
+        
     }
     db.close();
 }
 
-addblock();
+addTransactions();
